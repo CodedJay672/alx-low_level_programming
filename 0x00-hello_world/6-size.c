89a90b40383d6583fc3a8ac3e8c1a0aff7b136fa
Codedjay672 <emmajoel6456@gmail.com>
2022-07-04 13:37:44 +0000

    updating script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	char variableName[] = {"char", "int", "long int", "long long int", "float"};
-
-	for (int i = 0; i <= 5; i++)
-	{
-		printf("size of a %s: %ld\n", variableName[i], (sizeof(variableName[i])));
-	}
+		printf("size of a char: %ld byte(s)\n", sizeof(char));
+		printf("size of an int: %ld byte(s)\n", sizeof(int));
+		printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+		printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+		printf("size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 }
