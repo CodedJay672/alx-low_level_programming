85770716e243a60519eaa3c01024a13d30048cc3
Codedjay672 <emmajoel6456@gmail.com>
2022-07-04 19:59:49 +0000

    updating script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	printf("and that piece of art is usefle\" - Dora Korper, 2015-10-19\n");
+	write(2, "and tha piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
