c03a884fd6effef10329a7b76d1039f6ce1fee0c
Codedjay672 <emmajoel6456@gmail.com>
2022-07-05 23:04:06 +0000

    updating script

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,30 @@
 /**
  * print_to_98 - function that prins numbers to 98
  * @n: - integer argument to mark the start count value
- * 
  */
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n > 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("\n");
 	}
-	printf("\n");
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("%d, \n", n);
+	}
 }
