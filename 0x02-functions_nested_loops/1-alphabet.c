dfedd824da2ede99d5903e83c1aa4ac9a3dedbe0
Codedjay672 <emmajoel6456@gmail.com>
2022-07-05 13:02:53 +0000

    updating script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: - Always 0 (success)
+ */
+
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar(10);
+	return (0);
+}
