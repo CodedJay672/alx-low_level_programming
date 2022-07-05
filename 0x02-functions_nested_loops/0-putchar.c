18332f09ae15cc94561f6be23c8dd48214c8c318
Codedjay672 <emmajoel6456@gmail.com>
2022-07-05 11:54:51 +0000

    updating script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: - Always 0 (success)
+ */
+
+int main(void)
+{
+	write (1, "_putchar\n", 9);
+
+	return (0);
+}
