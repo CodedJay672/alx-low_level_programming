1b7abef142855cf768bec669ca7c2d8c705b4fbc
Codedjay672 <emmajoel6456@gmail.com>
2022-07-07 09:07:57 +0000

    updating script

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 	int i;
+
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
 
-	/*while (i < 10)
+	/**while (i < 10)
 	{
 		putchar(i);
 	}*/
 
-	printf("infinite loop avoided! \\0/\n");
+	printf("indefinite loop avoided! \\o/\n");
 
 	return (0);
 }
