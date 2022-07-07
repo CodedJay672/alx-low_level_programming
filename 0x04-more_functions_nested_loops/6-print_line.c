c6236d3b9e2b8d786c016b2f92b8fd7d803f6ea8
Codedjay672 <emmajoel6456@gmail.com>
2022-07-07 16:17:59 +0000

    updating script

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines in the terminal
+ * @n: function arguement
+ *
+ * Return: always void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
