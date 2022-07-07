f3d3b195eacca35132b721ca3c70682a5a53abe5
Codedjay672 <emmajoel6456@gmail.com>
2022-07-07 12:23:43 +0000

    updating script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first argument
+ * @b: second argument
+ * Return: Returns the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+	return (prod);
+}
