a528732c4600d32a7b51c59ef7c27e65711ac1e0
Codedjay672 <emmajoel6456@gmail.com>
2022-07-07 23:23:10 +0000

    updating script

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: - Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
