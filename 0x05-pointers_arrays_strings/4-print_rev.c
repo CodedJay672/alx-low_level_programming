76397a2c3436a9233c8ae83db3bde4a705ab3c4a
Codedjay672 <emmajoel6456@gmail.com>
2022-07-15 20:13:39 +0000

    updating script

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of the string
  * print_rev - prints strings in reverse
  * @s: string to print
  *
@@ -10,18 +9,14 @@
 
 void print_rev(char *s)
 {
-	int a = 0;
+	char *t = s;
 
-	while (*s)
+	if (s)
 	{
-		a = a + 1;
-		s++;
+		while (*t)
+			++t;
+		while (s < t--)
+			_putchar(*t);
+		_putchar('\n');
 	}
-	a = a - 1;
-	while (a >= 0)
-	{
-		_putchar(s[a]);
-		s--;
-	}
-	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,11 @@ void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
