094f2ebc01380d7d0c8bf84b281909566a2a2a55
Codedjay672 <emmajoel6456@gmail.com>
2022-07-16 01:45:34 +0000

    updating C functions

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 -function that encodes using rot13
+ * @s: funct argument
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *s)
+{
+	char *i;
+	char shift;
+
+	i = s;
+	while (*i)
+	{
+		shift = 'A' + (*i & 32);
+		if (('a' <= *i && *i <= 'z') || ('A' <= *i && *i <= 'Z'))
+			*i = (*i - shift + 13) % 26 + shift;
+		i++;
+	}
+	return(s);
+}
