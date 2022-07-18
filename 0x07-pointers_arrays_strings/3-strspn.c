89f3bd79b82d4ebace82c08f03f2918f6af015d0
Codedjay672 <emmajoel6456@gmail.com>
2022-07-18 21:18:58 +0000

    updating mem tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,24 +13,20 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int pos = 0;
 	int a = 0;
 
-	while (accept[a] != 0)
+	while (s[a] != 0)
 	{
 		int b = 0;
 
-		while (s[b] != 0)
+		while (accept[b] != 0)
 		{
-			if (accept[a] == s[b])
+			if (s[a] == accept[b])
 			{
-				if (pos < (unsigned int) b)
-				{
-					pos = b;
-				}
+				pos++;
 				break;
 			}
 			b++;
 		}
 		a++;
 	}
-	return (pos + 1);
+	return (pos);
 }
-
