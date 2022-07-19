1b96396a48641d7d28c6626d99af9ab77b18cbe6
Codedjay672 <emmajoel6456@gmail.com>
2022-07-19 12:29:18 +0000

    chessboard function

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: function argument
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int a = 0, j;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (a[i][j])
+		{
+			_putchar(a[i][j] + '0');
+			j++;
+		}
+		i++;
+	}
+}
