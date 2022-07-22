7897454a84bdd659bbf4feaa6de36e995114b2df
Codedjay672 <emmajoel6456@gmail.com>
2022-07-22 11:53:15 +0000

    updating function 191

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,38 +10,15 @@
 
 int wildcmp(char *s1,  char *s2)
 {
-	return (cmp_str(s1, s2, 0, 0, _strlen(s2)));
-}
-
-/**
- * _strlen - finds the length of a string
- * @s: string argument
- * Return: returns length of string
- */
-int _strlen(char *s)
-{
-	if (s == NULL)
-		return (0);
-	return (1 + _strlen(s + 1));
-}
-
-/**
- * cmp_str - compares two strings
- * @str1: first string
- * @str2: second string
- * @s1_strt: s1 start value
- * @s2_strt: s2 start value
- * @str2_len: length of str 2
- * Return: 1 if strings match else 0
- */
-int cmp_str(char *str1, char *str2, int s1_strt, int s2_strt, int str2_len)
-{
-	if (str2 == NULL)
-		return (0);
-	if (str2_len == 2 && str2[s2_strt] == '*')
-		return (1);
-	if (str2[s2_strt] == str1[s1_strt] || str2[s2_strt] == '*')
-		return (cmp_str(str1, str2, s1_strt, s2_strt + 1, str2_len));
-	else
-		return (cmp_str(str1, str2, s1_strt + 1, s2_strt, str2_len));
+	if (*s1 == NULL)
+	{
+		if (*s1 != NULL && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == NULL);
+	}
+	if (*s2 == '*')
+		return(wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
