b95920ce7b10c8d3b1099811f4597c9c2162f62d
Codedjay672 <emmajoel6456@gmail.com>
2022-07-23 00:11:54 +0000

    updated function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,16 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 1;
-	int product = 1;
+	int first, second, product;
 
-	if (argc <= 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	product = atoi(argv[1]) * atoi(argv[2]);
+	first = atoi(argv[1]);
+	second = atoi(argv[2]);
+	product = first * second;
 	printf("%d\n", product);
 	return (0);
 }
