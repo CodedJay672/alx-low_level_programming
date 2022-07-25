8cb14f0a1a4550d007baca63c56464d177eccaa0
Codedjay672 <emmajoel6456@gmail.com>
2022-07-25 02:49:14 +0000

    fixed malloc function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,19 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	char *ptr = malloc(sizeof(char) * size);
+	char *ptr;
+       
+	ptr = malloc(sizeof(char) * size);
 
-	if (size == 0 || !(ptr))
+	if (size == 0)
+		return (NULL);
+	if (ptr == NULL)
 		return (NULL);
 	while (i < size)
+	{
 		ptr[i] = c;
+		i++;
+	}
 	return (ptr);
 }
 
