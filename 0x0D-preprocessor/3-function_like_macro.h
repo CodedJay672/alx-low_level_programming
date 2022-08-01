4daf7359dbfab771b0f28ab2a9a0f191db38ad92
Codedjay672 <emmajoel6456@gmail.com>
2022-08-01 23:54:50 +0000

    update absolute macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,12 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ( \
-#if (x) < 0 \
-x = -1 * x \
-printf("%d\n", x) \
+#define ABS(x)\
+if ((x) < 0) \
+x = -1 * x; \
+printf("%d\n", x); \
 else \
-printf("%D\n", x) \
+printf("%d\n", x); \
 #endif \
-)
 
 #endif
