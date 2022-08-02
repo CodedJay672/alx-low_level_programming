51bca893773241ba4bd13b8960794318cf08b716
Codedjay672 <emmajoel6456@gmail.com>
2022-08-02 12:57:05 +0000

    update initialization funct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#define NULL 0
 
 /**
  * init_dog -> function that initializes the dog structure
@@ -10,6 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
