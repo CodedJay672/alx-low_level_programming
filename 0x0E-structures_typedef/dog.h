f7eab31cc4cc3c9de214981873e5364cb3a3078e
Codedjay672 <emmajoel6456@gmail.com>
2022-08-02 09:53:23 +0000

    update dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog -> structure definition of the dog type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
