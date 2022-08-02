8b33b344af320b24e1a40465451dcf795ecb81b7
Codedjay672 <emmajoel6456@gmail.com>
2022-08-02 13:16:01 +0000

    update print funct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * print_dog -> prints a struct dog
@@ -10,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name);
-		printf("Age: %d\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
