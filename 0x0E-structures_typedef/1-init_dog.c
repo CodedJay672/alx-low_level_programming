3bdd00b82e91edf1a3d9d9ddddf1b4a6950bd0b4
Codedjay672 <emmajoel6456@gmail.com>
2022-08-02 11:43:14 +0000

    initializing dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog -> function that initializes the dog structure
+ * @d: pointer declaration of dog data type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
