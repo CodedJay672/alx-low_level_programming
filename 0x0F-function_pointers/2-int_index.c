9869d50b34e725abfa5fbeb1baebdebba2aa1b97
Codedjay672 <emmajoel6456@gmail.com>
2022-08-03 20:43:47 +0000

    update compare pointer to function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * int int_index -> searches an array to find an ihteger match
+ * int_index -> searches an array to find an ihteger match
  * @array: array to search
  * @size: size of array
  * @cmp: function that performs the search initially
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int index = 0;
 
 	if (array == NULL || cmp == NULL)
-		return (0);
+		return (-1);
 	if (size == 0)
 		return (-1);
 	while (index < size)
