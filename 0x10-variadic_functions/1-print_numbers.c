80ec9892aef1c670e376a24d9b0eb74707961ba2
Codedjay672 <emmajoel6456@gmail.com>
2022-08-05 05:41:15 +0000

    updated print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,9 +21,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator == NULL)
 		{
 			printf("%d", va_arg(args, int));
-			if (i == (n - 1))
-				break;
-			printf(" ");
 		}
 		else
 		{
