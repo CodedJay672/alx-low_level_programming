0e23fd6f3a3e4b6170037b6eeb0091e2e313c6a7
Codedjay672 <emmajoel6456@gmail.com>
2022-08-12 00:52:29 +0000

    update function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * start -> prints a message before the main
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
