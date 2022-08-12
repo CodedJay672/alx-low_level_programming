9758a081e145347179c007b87089dba0c3e0b58d
Codedjay672 <emmajoel6456@gmail.com>
2022-08-12 00:11:16 +0000

    updated list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,21 +8,21 @@
 
 void free_list(list_t *head)
 {
-  list_t *temp = head;
+	list_t *temp = head;
 
-  if (head == NULL)
-    return;
-  
-  if (temp->next == NULL)
-  {
-    head = NULL;
-    return;
-  }
+	if (head == NULL)
+		return;
 
-  while (head !=NULL)
-  {
-    temp = head;
-    head = head->next;
-    free(temp);
-  }
+	if (temp->next == NULL)
+	{
+		free(temp);
+		return;
+	}
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
