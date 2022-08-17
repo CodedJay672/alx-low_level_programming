49c77d5ca2e62beb54fc106c04ac225957fb3068
Codedjay672 <emmajoel6456@gmail.com>
2022-08-17 03:49:37 +0000

    updated insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		node = node->next;
 		count++;
 	}
-	if (node == NULL && count == idx)
+	if (count == idx)
 	{
 		new_node->n = n;
 		new_node->next = node;
