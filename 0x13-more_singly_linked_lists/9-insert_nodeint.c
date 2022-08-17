c50122f2daa820815f5e7528f72ea2eee2b2e259
Codedjay672 <emmajoel6456@gmail.com>
2022-08-17 03:38:48 +0000

    updated insert funct

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,29 +13,35 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *temp;
-	unsigned int count = 1;
+	listint_t *node = (*head);
+	unsigned int count = 0;
 
 	new_node = (listint_t *)malloc(sizeof(listint_t));
-	new_node->n = n;
 	if (new_node == NULL)
 	{
 		free((*head));
 		return (NULL);
 	}
-	temp = (*head);
-	while (count < idx)
+	if ((*head) != NULL && idx == 0)
 	{
-		if (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		else
-		{
-			return (NULL);
-		}
+		new_node->n = n;
+		new_node->next = (*head);
+		(*head) = new_node;
+		return (new_node);
+	}
+	while ((*head) != NULL && count < idx)
+	{
+		temp = node;
+		node = node->next;
 		count++;
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	if (count == idx)
+	{
+		new_node->n = n;
+		new_node->next = node;
+		temp->next = new_node;
+		return (new_node);
+	}
+	else
+		return (NULL);
 }
