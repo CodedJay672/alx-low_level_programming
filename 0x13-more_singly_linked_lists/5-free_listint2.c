c0a7991f90db71aa7383ce5fa09ba3d9c7c85252
Codedjay672 <emmajoel6456@gmail.com>
2022-08-18 04:45:30 +0000

    update free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	
+
 	if (*head == NULL || head == NULL)
-	       return;
+		return;
 	while ((*head)->next != NULL)
 	{
 		temp = (*head)->next;
