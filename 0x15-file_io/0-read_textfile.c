881e7b0c174c8175049b63153c1a1fb6c6aed70b
Codedjay672 <emmajoel6456@gmail.com>
2022-08-22 16:28:07 +0000

    first file funct

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile -> funct that reads a text file and prints it to stdout
+ * @filename: this is the name of the file
+ * @letters: the number of characters to read and write
+ * Return: returns the number of bytes read and written
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buff[letters];
+	int fp, rp, wp;
+
+	fp = open(filename, O_RDWR);
+	if (fp == -1)
+		return (0);
+	rp = read(fp, buff, letters);
+	if (rp == -1)
+		return (0);
+	wp = write(1, buff, rp);
+	if (wp == -1)
+		return (0);
+	return (wp);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+**WELCOME TO THE FILE MANIPULATION DIRECTORY**
+
+The C programming languge gives us different means to work with files . They include the open(), read(), write() and close functions. This functions are requested by the current program running a process. this request is then either granted or denied by the OS kernel.
+
+this directory contains .c files which demonstrate the different ways that files can be manipulated using the C programming language.
+
+C is fun!!!
