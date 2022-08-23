e7c471c103c93ef35521d026f0898470c0276a64
Codedjay672 <emmajoel6456@gmail.com>
2022-08-23 09:48:09 +0000

    updated copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -59,7 +59,7 @@ int cp(char *src, char *dest)
 	cl = close(fp);
 	if (cl == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+		dprintf(STDERR_FILENO, "Error: Can't");
 		exit(100);
 	}
 	fp = open(dest, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
@@ -74,7 +74,7 @@ int cp(char *src, char *dest)
 	cl = close(fp);
 	if (cl == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+		dprintf(STDERR_FILENO, "Error: Can't");
 		exit(100);
 	}
 	return (1);
