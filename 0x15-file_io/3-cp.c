97304ba17454bcae4329ff8a1c493f77d1b9ee57
Codedjay672 <emmajoel6456@gmail.com>
2022-08-23 00:16:32 +0000

    copy funct

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * main -> Entry point
+ * @argc: argument count
+ * @argv: array of argument strings
+ * Return: always 1 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp(argv[1], argv[2]);
+	return (0);
+}
+
+/**
+ * cp -> copies content from src file to dest file
+ * @src: source filename string
+ * @dest: destination filename string
+ * Return: returns integer value if successful
+ */
+
+int cp(char *src, char *dest)
+{
+	char *buffer;
+	int fp, rp, wp, cl;
+
+	buffer = (char *)malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (-1);
+	fp = open(src, O_RDONLY);
+	if (fp == -1)
+		return (-1);
+	rp = read(fp, buffer, 1024);
+	if (rp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+	cl = close(fp);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fp);
+		exit(100);
+	}
+	fp = open(dest, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fp == -1)
+		return (-1);
+	wp = write(fp, buffer, rp);
+	if (wp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+		exit(99);
+	}
+	cl = close(fp);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fp);
+		exit(100);
+	}
+	return (1);
+}
