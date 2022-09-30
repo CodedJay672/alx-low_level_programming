f68a170b1b395d3ef8ceeedec65f13dcf11b8600
Codedjay672 <emmajoel6456@gmail.com>
2022-09-30 17:16:30 +0000

    update dll function

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,40 +13,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *last_node = *h;
 	unsigned int count = 0;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	while (last_node != NULL)
+	while (last_node->next != NULL)
 	{
-		if (idx == count)
+		if (count == idx)
 		{
-			if (last_node->next == NULL)
-			{
-				new_node->next = NULL;
-				new_node->prev = last_node;
-				last_node->next = new_node;
-				return (new_node);
-			}
-			else if (last_node->prev == NULL)
-			{
-				new_node->next = last_node;
-				new_node->prev = NULL;
-				last_node->prev = new_node;
-				*h = new_node;
-				return (new_node);
-			}
-			else
-			{
-				new_node->next = last_node;
-				new_node->prev = last_node->prev;
-				last_node->prev->next = new_node;
-				last_node->prev = new_node;
-				return (new_node);
-			}
+			new_node->n = n;
+			new_node->next = last_node;
+			new_node->prev = last_node->prev;
+			last_node->prev->next = new_node;
+			last_node->prev = new_node;
+			return (new_node);
 		}
 		last_node = last_node->next;
 		count++;
 	}
+	if (count++ == idx)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
