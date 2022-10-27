9b38772e6b1cd8047a773ebcf5c14692cfc13afe
Codedjay672 <emmajoel6456@gmail.com>
2022-10-27 01:40:41 +0000

    betty chceck

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int count;
+	hash_table_t *table;
 
-
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
