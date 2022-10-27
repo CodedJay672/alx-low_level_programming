ea432fe519bdca4dfec9c0a86fcbd991979ff731
Codedjay672 <emmajoel6456@gmail.com>
2022-10-27 01:37:08 +0000

    update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int count;
 
 
-	hash_table_t *table = malloc(sizeof(hash_table));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 	{
