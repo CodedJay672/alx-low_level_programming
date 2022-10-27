d0245423d8531f2c7067b774dee500802c31db1f
Codedjay672 <emmajoel6456@gmail.com>
2022-10-27 01:32:30 +0000

    update hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function which creates a hash table
+ * @size: size of table
+ * Return: pointer to created table of NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int count;
+
+
+	hash_table_t *table = malloc(sizeof(hash_table));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (count = 0; count < size; count++)
+	{
+		table->array[count] = NULL;
+	}
+	return (table);
+}
