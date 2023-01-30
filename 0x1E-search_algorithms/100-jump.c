2b5ed5e4b5a278337f86368e76629c1a0a146898
codedjay672 <emmajoel672@gmail.com>
2023-01-30 02:49:27 +0000

    update jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,23 +2,6 @@
 #include "search_algos.h"
 
 /**
- * min_val -> finds the minimum between two numbers
- * @a: first arg
- * @b: second arg
- *
- *
- * Return: returns minimum value
- */
-
-size_t min_val(int a, int b)
-{
-	if (a > b)
-		return (b);
-	else
-		return (a);
-}
-
-/**
  * jump_search -> function to search for a key using
  * the jump search algorithm
  *
@@ -41,21 +24,34 @@ int jump_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (array[min_val(step, size) - 1] < value)
+	while (array[step] < value)
 	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 		prev = step;
 		step += sqrt(size);
-		if (prev >= size)
+		if (prev >= size - 1)
+		{
+			printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+			printf("Value found between [%ld] and [%ld]\n", prev, step);
+			printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 			return (-1);
+		}
 	}
+
+	printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+	printf("Value found between [%ld] and [%ld]\n", prev, step);
 
 	while (array[prev] < value)
 	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 		prev++;
-		if (prev == min_val(step, size))
-			return (-1);
+		if (array[prev] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n",
+				prev, array[prev]);
+			return (prev);
+		}
 	}
-	if (array[prev] == value)
-		return (prev);
+
 	return (-1);
 }
