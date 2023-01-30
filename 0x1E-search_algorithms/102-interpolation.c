75d64c5cfca1dace0dccb5bab6bcda5e47f8b47c
codedjay672 <emmajoel672@gmail.com>
2023-01-30 04:06:42 +0000

    update search and interpolation search files

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -33,7 +33,7 @@ int interpolation_search(int *array, size_t size, int value)
 			break;
 		}
 		else
-			printf(" = [%d]", array[pos]);
+			printf(" = [%d]\n", array[pos]);
 
 		if (array[pos] == value)
 			return ((int)pos);
